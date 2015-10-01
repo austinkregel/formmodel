e7787ddffa1da366e8e6320b8dc6c22c582406b0
Austin Kregel <austinkregel@gmail.com>
2015-10-01 13:53:27 -0400

    Update FormModel, remove passwords from passing in and printing out in the values field

--- src/FormModel/FormModel.php
+++ src/FormModel/FormModel.php
@@ -155,7 +155,7 @@ class FormModel {
               'name' => $old_input,
               'placeholder' => $this->inputToRead($old_input),
               'label' => $this->inputToRead($old_input),
-              'value' => !empty($model->$input)?$model->$input:''
+              'value' => !empty($model->$input) &&(stripos($input, 'password') === false)?$model->$input:''
             ]);
           }
       } else { //not bootstrap

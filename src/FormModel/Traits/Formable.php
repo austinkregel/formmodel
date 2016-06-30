b9ef2768549b3484b24a6eac7a55b3413a4e0286
Austin Kregel <austinkregel@gmail.com>
2016-06-30 16:33:17 -0400

    Add Formable trait

---
+++ src/FormModel/Traits/Formable.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Kregel\FormModel\Traits;
+
+class FormModel
+{
+    protected $form_name;
+
+    /**
+     * @return mixed
+     */
+    public function getFormName()
+    {
+        return $this->form_name;
+    }
+
+    /**
+     * @param mixed $form_name
+     */
+    public function setFormName($form_name)
+    {
+        $this->form_name = $form_name;
+    }
+
+}

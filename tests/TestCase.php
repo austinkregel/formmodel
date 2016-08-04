bc2821c6af0bb0debd1314cabc0a72b530de14c4
Austin Kregel <austin@kbcomp.co>
2016-08-04 00:25:47 -0400

    Add tests

---
+++ tests/TestCase.php
@@ -0,0 +1,58 @@
+<?php
+require 'vendor/autoload.php';
+use Kregel\FormModel\FormModel;
+use Mockery as m;
+abstract class TestCase extends PHPUnit_Framework_TestCase
+{
+    protected $formmodel = null;
+
+    public function setUp()
+    {
+
+    }
+
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    protected function formModel()
+    {
+        return new FormModel();
+    }
+}
+
+if(!function_exists('config'))
+{
+    function config()
+    {
+
+    }
+}
+if(!function_exists('is_html'))
+{
+    function is_html($string)
+    {
+        return preg_match("/<[^<]+>/",$string,$m) != 0;
+    }
+}
+if(!function_exists('view'))
+{
+    function view($view, $data){
+        // This is just a quick hack because of course views are going to
+        // Spit out real html.... Pfff....
+        return '<form>'. $data['type']. $data['form_']. '</form>';
+    }
+}
+
+if(!function_exists('auth')){
+    function auth(){
+        return new class {
+            public function user(){
+                return new class extends \Illuminate\Database\Eloquent\Model {
+
+                };
+            }
+        };
+    }
+}
---
+++ tests/TestFrameworks.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: austinkregel
+ * Date: 8/3/16
+ * Time: 10:40 PM
+ */
+class TestFrameworks extends TestCase
+{
+
+    public function test_can_create_new_form_model_instance()
+    {
+        $this->formModel();
+    }
+
+    public function test_can_use_bootstrap_formmodel_framework()
+    {
+        $this->assertTrue(
+            $this->can_i_use_a_formmodel_framework('bootstrap')
+        );
+    }
+
+    public function test_can_use_bootstrap_vue_formmodel_framework()
+    {
+        $this->assertTrue(
+            $this->can_i_use_a_formmodel_framework('bootstrap-vue')
+        );
+    }
+
+    public function test_can_use_materialize_formmodel_framework()
+    {
+        $this->assertTrue(
+            $this->can_i_use_a_formmodel_framework('materialize')
+        );
+    }
+    public function test_can_use_materialize_vue_formmodel_framework()
+    {
+        $this->assertTrue(
+            $this->can_i_use_a_formmodel_framework('materialize-vue')
+        );
+    }
+
+    private function can_i_use_a_formmodel_framework($framework)
+    {
+        $form = $this->formModel();
+        $form->using($framework);
+        return true;
+    }
+
+    public function test_can_tell_if_frameworks_are_broken_dynamically(){
+        $this->should_tell_if_frameworks_are_broken('bootstrap');
+        $this->should_tell_if_frameworks_are_broken('bootstrap-vue');
+        $this->should_tell_if_frameworks_are_broken('materialize-vue');
+        $this->should_tell_if_frameworks_are_broken('materialize');
+    }
+    private function should_tell_if_frameworks_are_broken($framework)
+    {
+        echo $framework;
+        $model = new class extends \Illuminate\Database\Eloquent\Model {
+            protected $fillable = [
+                'name', 'project_id', 'ping_to'
+            ];
+            public function project(){
+                return $this->hasMany('App\Projects');
+            }
+        };
+        $form = $this->formModel()
+            ->using($framework)
+            ->withModel($model)
+            ->submitTo('/some/test/path')
+            ->form([
+                'method' => 'post',
+                'enctype' => 'multipart/form-data'
+            ]);
+        $this->assertTrue(is_html($form));
+    }
+}

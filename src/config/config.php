805054fae1c29ce3675b0718350d71200d1847de
Austin <austinkregel@users.noreply.github.com>
2016-08-05 00:40:52 -0400

    Applied fixes from StyleCI (#13)

--- src/config/config.php
+++ src/config/config.php
@@ -2,17 +2,17 @@
 
 return [
     'using' => [
-        'csrf' => true,
-        'framework' => 'bootstrap',
+        'csrf'             => true,
+        'framework'        => 'bootstrap',
         'custom-framework' => function () {
             return new Path\To\My\Framework();
         },
     ],
-    /**
+    /*
      * @param String
      * @return Collection
      */
-    'resolved_relationship' => function($class_name) {
+    'resolved_relationship' => function ($class_name) {
         return $class_name::limit(15)->get();
-    }
+    },
 ];

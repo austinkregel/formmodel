774a7f8c73b7b26038a4f1a28a1ab2d326ce2007
Austin Kregel <austin@kbcomp.co>
2016-08-05 00:39:14 -0400

    Update config

--- src/config/config.php
+++ src/config/config.php
@@ -1,11 +1,18 @@
 <?php
 
 return [
-  'using' => [
-    'csrf'             => true,
-    'framework'        => 'bootstrap',
-    'custom-framework' => function () {
-        return new Path\To\My\Framework();
-    },
-  ],
+    'using' => [
+        'csrf' => true,
+        'framework' => 'bootstrap',
+        'custom-framework' => function () {
+            return new Path\To\My\Framework();
+        },
+    ],
+    /**
+     * @param String
+     * @return Collection
+     */
+    'resolved_relationship' => function($class_name) {
+        return $class_name::limit(15)->get();
+    }
 ];

a3916cbbcac106501e8dc3701b3769b3e2999eaa
Austin Kregel <austinkregel@gmail.com>
2016-08-09 16:26:18 -0400

    Update Frameworks to reflect new changes from Plain framework

--- src/FormModel/Frameworks/Bootstrap.php
+++ src/FormModel/Frameworks/Bootstrap.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Kregel\FormModel\Interfaces\FrameworkInputs;
 use Kregel\FormModel\Interfaces\FrameworkInterface;
 
-class Bootstrap extends FrameworkInputs implements FrameworkInterface
+class Bootstrap extends Plain
 {
     /**
      * Generate the form.
@@ -42,121 +42,6 @@ class Bootstrap extends FrameworkInputs implements FrameworkInterface
             'class' => 'btn btn-primary pull-right',
         ], $options)).'</div>
         </div>';
-    }
-
-    /**
-     * # N/A request
-     * This will allow you to get the proper input type for an HTML form.
-     * It will extract the names from the a model.
-     *
-     * TODO: Clean up the methods. Shrink the size of this. To much for one method.
-     *
-     * @param string $input
-     * @param array  $input
-     * @param bool   $edit
-     *
-     * @return string (an HTML input element)
-     */
-    public function modelInput($input, $old_input = null, $edit = false)
-    {
-        $type = $this->getInputType($input, $old_input, $edit);
-        if (strlen($type) > 12) {
-            if (stripos($input, '_id') !== false) {
-                if (!empty(config('kregel.warden.models'))) {
-                    // Check if Warden exists
-                    $name = trim($input, '_id');
-                    $options = (auth()->user()->$name !== null) ? auth()->user()->$name : $this->model->$name;
-                    /* grab the model relation. what to do if there is no relation?
-  185036112                      convert it to a collection later on...*/
-                    if (empty($options)) {
-                        $model = config('kregel.warden.models.'.$name.'.model');
-                        if (!empty($model)) {
-                            $options = $model::all();
-                        }
-                    }
-                } else {
-                    $options = (auth()->user()->$input !== null) ? auth()->user()->$input : $this->model->$input;
-                }
-                $ops = [];
-                if (!$options instanceof Collection && !empty($options)) {
-                    $options = collect([$options]);
-                }
-                if (!empty($options)) {
-                    if (!$options->isEmpty()) {
-                        foreach ($options as $option) {
-                            $this->accessor = !empty($option->getFormName()) ? $option->getFormName() : 'name';
-
-                            $ops[$option->id] = ucwords(preg_replace('/[-_]+/', ' ', $option->{$this->accessor}));
-                        }
-                        $relation = $this->model->{trim($input, '_id')};
-                        if ($relation instanceof Collection && $relation->count() === 1) {
-                            $default = $relation->first();
-                        } else {
-                            $default = empty($this->model->{trim($input, '_id')}->id) ? '' : $this->model->{trim($input, '_id')}->id;
-                        }
-
-                        return $this->select([
-                            'default_text' => 'Please select a '.trim($input, '_id').' to assign this to',
-                            'default'      => empty($default) ? '' : $default,
-                            'type'         => 'select',
-                            'class'        => 'form-control',
-                            'name'         => $input,
-                            'id'           => $this->genId($input),
-                        ], $ops);
-                    }
-                }
-            }
-        }
-        if ($type === 'select') {
-            return $this->select([
-                'default_text' => 'Please select a '.trim($input, '_id'),
-                'type'         => $type,
-                'name'         => $input,
-                'id'           => $this->genId($input),
-            ], [
-                false => 'No',
-                true  => 'Yes',
-            ]);
-        } elseif (in_array($type, [
-            'text',
-        ])) {
-            return $this->textarea([
-                'type'    => $type,
-                'name'    => $input,
-                'id'      => $this->genId($input),
-            ], (!empty($this->model->$input) && !(stripos($input,
-                        'password') !== false)) ? $this->model->$input : '');
-        } elseif ($type === 'file') {
-            $label = (!empty($options['name']) ? ucwords($options['name']) : '');
-            $returnable = '<div class="file-field input-field"><div class="btn"><span>Your file</span>
-                '.parent::plainInput([
-                    'type'     => $type,
-                    'name'     => $input,
-                    'class'    => 'form-control',
-                    'id'       => $this->genId($label),
-                    'multiple' => '',
-                ]).(empty($label) | (substr($label, 0,
-                        1) == '_') ? '' : '<label for="'.$this->genId($label).'">'.$label.'</label>').'
-                </div>
-                <div class="file-path-wrapper">
-                <input class="file-path validate" type="text" placeholder="Upload one or more files">
-              </div>
-            </div>';
-
-            return $returnable;
-        } elseif (in_array($type, [
-            'password',
-            'email',
-            'date',
-            'number',
-        ])) {
-            return $this->input([
-                'type'    => $type,
-                'name'    => $input,
-                'class'   => 'form-control',
-                'id'      => $this->genId($input),
-            ]);
-        }
     }
 
     public function select(array $configs, array $options)
